29da10d8fd5b155098f3ef2ee2080520f0dde5e9
Zaicon Kiroshu <kzaicon@yahoo.com>
2016-12-26 15:55:06 -0600

    Mintaka Pre-Release 2 Build

---
+++ AIO/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using TShockAPI;
+
+namespace AIO
+{
+	public static class Utils
+	{
+		private static string filepath = Path.Combine("tshock", "logs");
+
+		public static string GetPath()
+		{
+			return Path.Combine(filepath, DateTime.Now.ToString("yyyy-MM-dd") + ".log");
+		}
+
+		public static string Specifier(this bool silent)
+		{
+			if (silent)
+				return TShock.Config.CommandSilentSpecifier;
+			else
+				return TShock.Config.CommandSpecifier;
+		}
+	}
+}
